b3a1efbb59dcfe4c19755b7e986c82999eb7854a
Sherif Radwan <dr.sherif1992@gmail.com>
2018-06-09 08:51:56 +0200

    configure flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
+# imports
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # Flask application instance
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# DataBase instance
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# flask-login configuration
+login = LoginManager(app)
+login.login_view = 'auth.login'
+login.init_app(app)
 
 # register the Blueprints occur here
 from . import auth
 app.register_blueprint(auth.bp)
-
-# DataBase instance
 
 from . import models, routes
