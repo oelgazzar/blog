a633b011527ab08768066e98b255b3d056910a57
Sherif Radwan <dr.sherif1992@gmail.com>
2018-06-16 20:54:32 +0200

    FLASK_WTF

--- app/auth.py
+++ app/auth.py
@@ -5,45 +5,40 @@ from flask import render_template, redirect, url_for, request, Blueprint, flash,
 from flask_login import current_user, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.urls import url_parse
+from .forms import AuthForm
 
 # define blueprint for urls related to authentication
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-"""
+
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
+    """
+    a view for registering the user
+    When GET:
+            return the register page
+    When POST:
+            processing data after submitting and if valid save it to the database
+    if there is any error return to register page and flash the error
+    """
 
-# a view for registering the user
-# When GET:
-#     return the register page
-# When POST:
-#     processing data after submitting and if valid save it to the database
-# if there is any error return to register page and flash the error
-
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    form = AuthForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
         identical_user = User.query.filter(User.username == username).first()
         error = None
 
-        if not username:
-            error = 'Username field is qequired'
-        elif identical_user is not None:
-            error = 'This username is already registered'
-        elif not password:
-            error = 'password field is required'
+        if identical_user is not None:
+            flash('UserName is already registered')
+            return redirect(url_for('auth.register'))
 
-        if error is not None:
-            return render_template('auth/register.html', error=error)
-        else:
-            new_user = User(username=username, password=generate_password_hash(password))
-            db.session.add(new_user)
-            db.session.commit()
-
-            return redirect(url_for('auth.login'))
+        new_user = User(username=username, password=generate_password_hash(password))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
     else:
-        return render_template('auth/register.html')
-"""
+        return render_template('auth/register.html', form=form)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -55,34 +50,33 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
 
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    form = AuthForm(request.form)
+    if request.method == 'POST' and form.validate():
+        username = form.username.data
+        password = form.password.data
         user = User.query.filter_by(username=username).first()
         all_identical_users = User.query.filter(User.username == username).first()
         error = None
 
-        if not username:
-            error = 'USERNAME is required Ya Dog :) '
+        # if not username:
+        #     error = 'USERNAME is required Ya Dog :) '
 
-        elif all_identical_users is None:
-            error = 'User Not Registered'
-        elif not check_password_hash(user.password, password):
-            error = 'Incorrect password Ya Animal :) '
+        # if not user:
+        #     flash('User Not Registered')
+        #     return redirect(url_for('auth.login'))
 
-        if error is not None:
-            flash(error)
-            return render_template('auth/login.html', error=error,
-                                   all_identical_users=all_identical_users)
-        else:
-            # login and remember the user
-            login_user(user, remember=True)
-            next_page = request.args.get('next')
-            if not next_page or url_parse(next_page).netloc != '':
-                next_page = url_for('index')
-            return redirect(next_page)
+        if not check_password_hash(user.password, password):
+            flash('Incorrect password or username Ya Animal :) ')
+            return redirect(url_for('auth.login'))
 
-    return render_template('auth/login.html')
+        # login and remember the user
+        login_user(user, remember=True)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+
+    return render_template('auth/login.html', form=form)
 
 
 @bp.route('/logout')
