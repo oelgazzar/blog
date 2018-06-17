20d8dee70a2f6b4db3b8b5fdd592f376004987a3
Sherif Radwan <dr.sherif1992@gmail.com>
2018-06-17 06:27:21 +0200

    re-stop register

--- app/auth.py
+++ app/auth.py
@@ -10,15 +10,15 @@ from .forms import AuthForm
 # define blueprint for urls related to authentication
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-
+"""
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     """
     a view for registering the user
     When GET:
-            return the register page
+        return the register page
     When POST:
-            processing data after submitting and if valid save it to the database
+        processing data after submitting and if valid save it to the database
     if there is any error return to register page and flash the error
     """
 
@@ -39,6 +39,7 @@ def register():
         return redirect(url_for('auth.login'))
     else:
         return render_template('auth/register.html', form=form)
+"""
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -58,12 +59,9 @@ def login():
         all_identical_users = User.query.filter(User.username == username).first()
         error = None
 
-        # if not username:
-        #     error = 'USERNAME is required Ya Dog :) '
-
-        # if not user:
-        #     flash('User Not Registered')
-        #     return redirect(url_for('auth.login'))
+        if not user:
+            flash('User Not Registered')
+            return redirect(url_for('auth.login'))
 
         if not check_password_hash(user.password, password):
             flash('Incorrect password or username Ya Animal :) ')
